4c196350ef514540c34e80c75afe6a3140121a0d
kyunooh <chm073@gmail.com>
2015-11-27 22:58:17 +0900

    update setting and build model with test

--- checker/tests.py
+++ checker/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from .models import Users
+from .models import User
 
 class DBTest(TestCase):
 
     def test_user_model_save_and_get_data(self):
-        User.create(nickname="nickname")
+        User.objects.create(nickname="nickname")
         all_users = User.objects.all()
-        self.asesrtTrue(all_users)
+        self.assertTrue(all_users)
