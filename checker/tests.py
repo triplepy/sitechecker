82554d918b00d3148e3a2f15e790aa07a76d3eaa
kyunooh <chm073@gmail.com>
2015-11-27 22:49:17 +0900

    add DBTest

--- checker/tests.py
+++ checker/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Users
 
-# Create your tests here.
+class DBTest(TestCase):
+
+    def test_user_model_save_and_get_data(self):
+        User.create(nickname="nickname")
+        all_users = User.objects.all()
+        self.asesrtTrue(all_users)
