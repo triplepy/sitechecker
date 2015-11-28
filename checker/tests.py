deb6a9d3cd99dd97cda58776dda34326689db1b0
ChoiHyeonMook <chm073@gmail.com>
2015-11-28 12:31:15 +0900

    Add Site model and test

--- checker/tests.py
+++ checker/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import User
+from .models import User, Site
 
 class DBTest(TestCase):
 
@@ -7,3 +7,12 @@ class DBTest(TestCase):
         User.objects.create(nickname="nickname")
         all_users = User.objects.all()
         self.assertTrue(all_users)
+
+    def test_site_model_save_and_get_data(self):
+        some_user = User.objects.create(nickname="site_model_test")
+        Site.objects.create(user=some_user)
+        all_sites = Site.objects.all()
+        self.assertTrue(all_sites)
+
+
+        
