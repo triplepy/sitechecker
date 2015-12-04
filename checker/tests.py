ae35ff036f8f5e313388c383a51d881637d9c55b
ChoiHyeonMook <chm073@gmail.com>
2015-12-04 12:23:13 +0900

    update post request about home

--- checker/tests.py
+++ checker/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
+from django.test.client import RequestFactory
 from .models import User, Site
-
+from .views import home
 
 class DBTest(TestCase):
 
@@ -24,3 +25,14 @@ class HomePageTest(TestCase):
         response = self.c.get('/')
         self.assertEquals(response.status_code, 200)
         self.assertIn("Hello", response.content.decode())
+
+class PostDataTest(TestCase):
+    def setUp(self):
+        self.factory = RequsetFactory()
+    
+    def test_post_data(self):
+        request.factory.post('/')
+        response = home(request)
+        self.assertEquals(200, response.status_code)
+        all_users = User.objects.all()
+        self.assertTrue(all_users)
