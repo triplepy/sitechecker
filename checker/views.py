a476e11bcf94d5d9833084f12851bdec08cf45db
ChoiHyeonMook <chm073@gmail.com>
2015-12-05 09:25:34 +0900

    update register form

--- checker/views.py
+++ checker/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
-
 def home(request):
+    if request.method == 'POST':
+       user =  User.create(nickname=request['nickname'])
+       Site.create(user=user, url=request['siteurl'])
+       
     return render(request,'checker/home.html', '')
 
 
