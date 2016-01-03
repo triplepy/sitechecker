da32a08968831fbde691625a0c057b5847401061
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2016-01-03 18:29:38 +0900

    add for certify info

--- checker/models.py
+++ checker/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-
+import uuid
 
 class User(models.Model):
     nickname = models.CharField(max_length=50, unique=True)
+    
 
 
 class Site(models.Model):
     user = models.ForeignKey("User")
     url = models.CharField(max_length=300)
+    uuid_to_ceitify = models.CharField(max_length=70, default=str(uuid.uuid4()))
+    is_certified = models.BooleanField(default=False)
 
     def send_register_mail(self):
-        # TODO 구현 필요
+        
         pass
+
